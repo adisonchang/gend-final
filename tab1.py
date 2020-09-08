93127faae54dd693075ca05d511eca6646bb9f3a
adisonchang <68548118+adisonchang@users.noreply.github.com>
2020-09-08 13:49:56 -0700

    Update tab1.py

--- tab1.py
+++ tab1.py
@@ -1,3 +1,6 @@
+# This file sets the layout for the 1st tab of the webpage
+# Contains 4 large numbers with mail-in ballot info
+
 import plotly.graph_objects as go
 import dash_core_components as dcc
 import dash_html_components as html
@@ -10,8 +13,10 @@ ttl_rem = sum(mail_tots['Remaining'])
 dem_ret = mail_tots.loc[mail_tots['Party'] == 'DEMOCRAT', 'Returned'].item()
 dem_rem = mail_tots.loc[mail_tots['Party'] == 'DEMOCRAT', 'Remaining'].item()
 
+# Initialize Dash Figure
 fig = go.Figure()
 
+# Adding first number
 fig.add_trace(go.Indicator(
     mode="gauge+number",
     value=ttl_ret,
@@ -19,6 +24,7 @@ fig.add_trace(go.Indicator(
     gauge={'axis': {'visible': False}},
     domain={'row': 1, 'column': 0}))
 
+# Second number
 fig.add_trace(go.Indicator(
     mode="gauge+number",
     value=dem_ret,
@@ -27,6 +33,7 @@ fig.add_trace(go.Indicator(
     domain={'row': 1, 'column': 1}
 ))
 
+# Third number
 fig.add_trace(go.Indicator(
     mode="number",
     value=ttl_rem,
@@ -34,6 +41,7 @@ fig.add_trace(go.Indicator(
     domain={'row': 3, 'column': 0}
 ))
 
+# Fourth number
 fig.add_trace(go.Indicator(
     mode="number",
     value=dem_rem,
@@ -41,6 +49,7 @@ fig.add_trace(go.Indicator(
     domain={'row': 3, 'column': 1}
 ))
 
+# Set some design information
 fig.update_layout(
     grid={'rows': 4, 'columns': 2, 'pattern': "independent"},
     margin=dict(
@@ -51,6 +60,7 @@ fig.update_layout(
         pad=4)
 )
 
+# Finalize tab layout
 layout = html.Div(children=[
     html.Br(),
     html.H2(
