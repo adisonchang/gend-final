558c56436316d3e802381b46d9610c10f1e275ad
adisonchang <68548118+adisonchang@users.noreply.github.com>
2020-09-08 13:55:00 -0700

    Update tab3.py

--- tab3.py
+++ tab3.py
@@ -1,9 +1,17 @@
+# tab3.py builds a scatterplot for the 3rd tab of the webpage
+# The scatter shows how many ballots have been returned by county
+
 import dash_core_components as dcc
 import dash_html_components as html
 import plotly.express as px
 
 from data import mail_county
 
+# Initialize Plotly Scatterplot
+# Each county is a different bubble, and they are 
+# Color-coded based on how many votes are remaining
+# X-axis is log(Ballots Issued) and
+# Y-axis is log(Ballots Returned)
 fig = px.scatter(mail_county, x="Issued", y="Returned",
                  opacity=0.7,
                  hover_name="County",
@@ -15,6 +23,11 @@ fig.update_traces(marker=dict(size=12,
                                         color='black')),
                   selector=dict(mode='markers'))
 
+# Initialize Plotly Scatterplot
+# Each county is a different bubble, and they are 
+# Color-coded based on how many votes are remaining
+# X-axis is Ballots Issued and
+# Y-axis is Ballots Returned
 fig2 = px.scatter(mail_county, x="Issued", y="Returned",
                   opacity=0.7,
                   hover_name="County",
@@ -25,6 +38,7 @@ fig2.update_traces(marker=dict(size=12,
                                          color='black')),
                    selector=dict(mode='markers'))
 
+# Finalize tab layout 
 layout = html.Div(children=[
     html.Br(),
     html.H2(children='Counties by Ballots Issued and Ballots Returned (log)',
