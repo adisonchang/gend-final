875f83a7fad7116aeb72177636a965cf1e62e06c
adisonchang <68548118+adisonchang@users.noreply.github.com>
2020-09-08 13:52:19 -0700

    Update tab2.py

--- tab2.py
+++ tab2.py
@@ -1,3 +1,6 @@
+# Tab2.py sets the layout for the 2nd tab of the webpage
+# Four numbers that exhibit how many votes have been returned
+
 import plotly.graph_objects as go
 import dash_core_components as dcc
 import dash_html_components as html
@@ -9,8 +12,10 @@ ttl_rem = sum(tot_tots['Remaining'])
 dem_ret = tot_tots.loc[tot_tots['Party'] == 'DEMOCRAT', 'Returned'].item()
 dem_rem = tot_tots.loc[tot_tots['Party'] == 'DEMOCRAT', 'Remaining'].item()
 
+# Initialize Dash Figure
 fig = go.Figure()
 
+# Set 1st number: Total Ballots Returned
 fig.add_trace(go.Indicator(
     mode="gauge+number",
     value=ttl_ret,
@@ -18,6 +23,7 @@ fig.add_trace(go.Indicator(
     gauge={'axis': {'visible': False}},
     domain={'row': 1, 'column': 0}))
 
+# 2nd number: Total Democratic Ballots Returned
 fig.add_trace(go.Indicator(
     mode="gauge+number",
     value=dem_ret,
@@ -26,6 +32,7 @@ fig.add_trace(go.Indicator(
     domain={'row': 1, 'column': 1}
 ))
 
+# 3rd number: Total Ballots Remaining / Yet to be returned
 fig.add_trace(go.Indicator(
     mode="number",
     value=ttl_rem,
@@ -33,6 +40,7 @@ fig.add_trace(go.Indicator(
     domain={'row': 3, 'column': 0}
 ))
 
+# 4th Number: Total Democratic Ballot Remaining
 fig.add_trace(go.Indicator(
     mode="number",
     value=dem_rem,
@@ -40,6 +48,7 @@ fig.add_trace(go.Indicator(
     domain={'row': 3, 'column': 1}
 ))
 
+# Set some design variables
 fig.update_layout(
     grid={'rows': 4, 'columns': 2, 'pattern': "independent"},
     margin=dict(
@@ -50,6 +59,7 @@ fig.update_layout(
         pad=4)
 )
 
+# Finalize tab layout 
 layout = html.Div(children=[
     html.Br(),
     html.H2(
